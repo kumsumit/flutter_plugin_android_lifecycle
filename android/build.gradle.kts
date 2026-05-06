203c946208848a9b9440e18f1a84f0a8c57eb1f1
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-06 19:44:07 +0530

    code updated

---
+++ android/build.gradle.kts
@@ -0,0 +1,40 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android") version "2.3.21"
+}
+
+group = "io.flutter.plugins.flutter_plugin_android_lifecycle"
+version = "1.0"
+
+repositories {
+        google()
+        mavenCentral()
+    }
+
+android {
+    namespace = "io.flutter.plugins.flutter_plugin_android_lifecycle"
+    compileSdk = 37
+
+    defaultConfig {
+        minSdk = 24
+        consumerProguardFiles = files('proguard.txt')
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    lintOptions {
+        checkAllWarnings = true
+        warningsAsErrors = true
+        disable = setOf('AndroidGradlePluginVersion', 'InvalidPackage', 'GradleDependency', 'NewerVersionAvailable')
+    }
+}
+
+dependencies {
+        implementation("androidx.annotation:annotation:1.10.0")
+    }
+
